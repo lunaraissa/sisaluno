544daac2103293b6d00afba982540600c85e4e4f
Luna Raíssa <130191119+lunaraissa@users.noreply.github.com>
2023-07-20 22:05:00 -0300

    Add files via upload

---
+++ listaalunos.php
@@ -0,0 +1,65 @@
+<?php 
+/*
+ * Melhor prática usando Prepared Statements
+ * 
+ */
+  require_once('conexao.php');
+   
+  $retorno = $conexao->prepare('SELECT * FROM aluno');
+  $retorno->execute();
+
+?>       
+        <table> 
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>ENDEREÇO</th>
+                    <th>TURMA</th>
+                    <th>DATA DE NASCIMENTO</th>
+                    <th>ENDEREÇO</th>
+                    <th>STATUS</th>
+
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <?php foreach($retorno->fetchall() as $value) { ?>
+                        <tr>
+                            <td> <?php echo $value['id'] ?>   </td> 
+                            <td> <?php echo $value['nome']?>  </td> 
+                            <td> <?php echo $value['idade']?> </td> 
+                            <td> <?php echo $value['datanas'] ?>   </td> 
+                            <td> <?php echo $value['turma']?>  </td> 
+                            <td> <?php echo $value['endereco']?> </td> 
+                            <td> <?php echo $value['estatus']?> </td> 
+
+
+                            <td>
+                               <form method="POST" action="altaluno.php">
+                                        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+                                        <button name="alterar"  type="submit">Alterar</button>
+                                </form>
+
+                             </td> 
+
+                             <td>
+                               <form method="GET" action="crudaluno.php">
+                                        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+                                        <button name="excluir"  type="submit">Excluir</button>
+                                </form>
+
+                             </td> 
+
+
+                       
+                      </tr>
+                    <?php  }  ?> 
+                 </tr>
+            </tbody>
+        </table>
+<?php         
+   echo "<button class='button button3'><a href='index.php'>voltar</a></button>";
+?>
